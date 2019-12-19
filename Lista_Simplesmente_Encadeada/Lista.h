fd3a56a0c0583ff659380444e0078a80e368165d
nardao666 <nardaocadastro@gmail.com>
2019-12-19 08:10:06 -0300

    Criado Lista.h

---
+++ Lista_Simplesmente_Encadeada/Lista.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//prototipos das funções que serão utilizados
+//nomes das funções
+//o que cada função faz esta escrita no Lista.c
+void Tamanho() ;
+
+void Inserir_frente();
+
+void Inserir_final();
+
+void Inserir_apos();
+
+void Remover_frente();
+
+void Remover_final();
+
+void Remover_apos();
